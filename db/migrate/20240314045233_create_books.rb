1af38b46fbc5bea8da3db053ce29be7645d922ba
Ryzhon <ryuta.hokazono@gmail.com>
2024-03-14 14:44:26 +0900

    add:本の表示、登録をする 詳細: バリデーションやBook APIの最終調整が必要

---
+++ db/migrate/20240314045233_create_books.rb
@@ -0,0 +1,15 @@
+class CreateBooks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.integer :genre_id
+      t.text :summary
+      t.string :status
+      t.string :isbn
+      t.string :imageUrl
+
+      t.timestamps
+    end
+  end
+end
