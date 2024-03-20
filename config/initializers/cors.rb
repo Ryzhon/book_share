84a5fde0d1e1a7ae2b957210f66bbd89a92bd07e
Ryzhon <ryuta.hokazono@gmail.com>
2024-03-20 13:45:27 +0900

    デプロイ環境整備

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'http://localhost:3000' # Reactアプリケーションのオリジン
-    resource '*', headers: :any, methods: %i[get post put patch delete options head]
-  end
-end
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'http://localhost:3000' # Reactアプリケーションのオリジン
+#     resource '*', headers: :any, methods: %i[get post put patch delete options head]
+#   end
+# end
