c2d06e7b0ffb5151d2ef9d588646a5334592f071
Ryzhon <ryuta.hokazono@gmail.com>
2024-03-22 16:10:40 +0900

    CSFR対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@
 
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token if Rails.env.development?
+  protect_from_forgery with: :null_session, if: -> { request.format.json? }
 end
